23f7f005a9e9cf6f9733932c0f1e7fc75a093033
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-07 13:45:12 +0100

    Add react-router & setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+
 export const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </div>
   );
 };
