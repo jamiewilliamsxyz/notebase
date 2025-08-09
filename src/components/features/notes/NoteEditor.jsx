b5840aee26d04a2663e637d93de598781cd196ca
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-09 10:15:53 +0100

    Add placeholder to textarea & ajust title input focused background colour
    Added a placeholder to help users know where to start typing in the page & Made the title inputs background slightly darker when focused

--- src/components/features/notes/NoteEditor.jsx
+++ src/components/features/notes/NoteEditor.jsx
@@ -4,13 +4,14 @@ export const NoteEditor = () => {
     <div className="min-h-screen flex flex-col p-10">
       <input
         type="text"
-        className="input input-ghost input-xl w-full h-32 p-10 text-5xl font-semibold focus:outline-none"
+        className="input input-ghost input-xl w-full h-32 p-10 text-5xl font-semibold focus:bg-base-200 focus:outline-none"
       />
       <textarea
         onInput={(e) => {
           e.target.style.height = "auto";
           e.target.style.height = e.target.scrollHeight + "px";
         }}
+        placeholder="Start typing here..."
         className="textarea textarea-xl textarea-ghost resize-none overflow-hidden w-full leading-relaxed p-10 pt-5 focus:bg-base-300 focus:outline-none min-h-[calc(100vh - 268px)]"
       />
     </div>
