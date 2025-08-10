9023fedc80767bcebac0a62edd2beede0ca8e166
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-10 12:33:44 +0100

    Move LayoutContextProvider to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { LayoutContextProvider } from "./context/LayoutContext";
+import { useContext } from "react";
+import { LayoutContext } from "./context/LayoutContext";
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { Sidebar } from "./components/layout/Sidebar";
 
 export const App = () => {
+  const { isExpanded } = useContext(LayoutContext);
+
   return (
-    <LayoutContextProvider>
-      <div className="flex">
-        <Sidebar />
-        <main className="ml-[20rem] w-full">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </main>
-      </div>
-    </LayoutContextProvider>
+    <div className="flex">
+      <Sidebar />
+      <main className="ml-[20rem] w-full">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/index.css";
+import { LayoutContextProvider } from "./context/LayoutContext";
 import { App } from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <LayoutContextProvider>
+        <App />
+      </LayoutContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
