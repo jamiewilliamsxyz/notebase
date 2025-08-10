d790594f7ad72a68d46e93b44fdaef3b08014d3c
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-10 12:56:06 +0100

    Change input & textarea background to transparent on focus

--- src/components/features/notes/NoteEditor.jsx
+++ src/components/features/notes/NoteEditor.jsx
@@ -1,10 +1,12 @@
 export const NoteEditor = () => {
   // https://daisyui.com/components/loading/
+  // Auto focus title when file is created
+
   return (
     <div className="min-h-screen flex flex-col p-10">
       <input
         type="text"
-        className="input input-ghost input-xl w-full h-32 p-10 text-5xl font-semibold focus:bg-base-200 focus:outline-none"
+        className="input input-ghost input-xl w-full h-32 p-10 text-5xl font-semibold focus:bg-transparent focus:outline-none"
       />
       <textarea
         onInput={(e) => {
@@ -12,7 +14,7 @@ export const NoteEditor = () => {
           e.target.style.height = e.target.scrollHeight + "px";
         }}
         placeholder="Start typing here..."
-        className="textarea textarea-xl textarea-ghost resize-none overflow-hidden w-full leading-relaxed p-10 pt-5 focus:bg-base-300 focus:outline-none min-h-[calc(100vh - 268px)]"
+        className="textarea textarea-xl textarea-ghost resize-none overflow-hidden w-full leading-relaxed p-10 pt-5 focus:bg-transparent focus:outline-none min-h-[calc(100vh - 268px)]"
       />
     </div>
   );
