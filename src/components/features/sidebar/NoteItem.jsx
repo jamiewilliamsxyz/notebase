ca20e337a6c025320cf4e8ecfdcca072ebeb0b43
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-11 20:07:29 +0100

    Create dropdown menu for NoteItem
    Created a basic dropdown menu for NoteItem, this feature is still incomplete

--- src/components/features/sidebar/NoteItem.jsx
+++ src/components/features/sidebar/NoteItem.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect } from "react";
 import { WorkspaceContext } from "../../../context/WorkspaceContext";
+import { Ellipsis, Trash, PenLine } from "lucide-react";
 
 export const NoteItem = ({ id, title }) => {
   const { openNote, noteOpen } = useContext(WorkspaceContext);
@@ -20,9 +21,29 @@ export const NoteItem = ({ id, title }) => {
         onClick={() => openNote(id)}
         className={`${
           isSelected ? "bg-primary" : ""
-        } mb-1.5 transition-colors duration-300 ease-in-out`}
+        } mb-1.5 transition-colors duration-300 ease-in-out dropdown dropdown-right flex flex-row justify-between`}
       >
         {title}
+        <div tabIndex={0} role="button">
+          <Ellipsis className="w-7 h-7" />
+        </div>
+
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow"
+        >
+          <li>
+            <a className="flex flex-row gap-3">
+              <PenLine className="w-6 h-6" /> Rename
+            </a>
+          </li>
+          <li>
+            <a className="flex flex-row gap-3">
+              <Trash className="w-6 h-6" />
+              Delete
+            </a>
+          </li>
+        </ul>
       </a>
     </li>
   );
