57c3f4b118c2f3987e170263d70770eec4628824
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-14 19:35:10 +0100

    Make editor mode toggle component responsive to theme change

--- src/components/features/editor/EditorModeToggle.jsx
+++ src/components/features/editor/EditorModeToggle.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
 import { PencilLine, BookOpen } from "lucide-react";
+import { ThemeContext } from "../../../context/ThemeContext";
 export const EditorModeToggle = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="fixed top-2 right-2 w-fit h-fit p-2">
+    <div
+      className={`fixed top-2 right-2 w-fit h-fit p-2 ${
+        theme === "dark" ? "" : "text-base-300"
+      }`}
+    >
       <label className="swap swap-rotate">
         <input type="checkbox" />
-        <PencilLine className="swap-on text-base-300 w-6 h-6" />
-        <BookOpen className="swap-off text-white w-6 h-6" />
+        <PencilLine className="swap-on w-6 h-6" />
+        <BookOpen className="swap-off w-6 h-6" />
       </label>
     </div>
   );
