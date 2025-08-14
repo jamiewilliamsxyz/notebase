0260749bee1e4fcda13742cd257658c535e59989
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-14 20:00:02 +0100

    Add conditional rendering for the editor modes

--- src/components/features/editor/MarkdownEditor.jsx
+++ src/components/features/editor/MarkdownEditor.jsx
@@ -27,12 +27,23 @@ export const MarkdownEditor = () => {
       }`}
     >
       <EditorModeToggle onToggleEditorMode={handleToggleEditorMode} />
-      <textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Start typing here..."
-        className="textarea text-base bg-transparent border-0 shadow-none resize-none overflow-hidden w-full min-h-96 leading-relaxed focus:bg-transparent focus:shadow-none focus:outline-none "
-      />
+
+      {editorMode === "edit" ? (
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Start typing here..."
+          className="textarea text-base bg-transparent border-0 shadow-none resize-none overflow-hidden w-full min-h-96 leading-relaxed focus:bg-transparent focus:shadow-none focus:outline-none "
+        />
+      ) : editorMode === "view" ? (
+        <div>HTML Content</div>
+      ) : (
+        <div className="flex items-center justify-center">
+          <p className="text-error text-3xl text-semibold">
+            Error: Invalid editor mode
+          </p>
+        </div>
+      )}
 
       <ThemeToggle />
     </div>
