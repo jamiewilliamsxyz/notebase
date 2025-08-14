d1c43a09aba8c5ee349700d3d21568a635bc48ee
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-14 12:46:27 +0100

    Add a minimum height to the text area

--- src/components/features/editor/NoteEditor.jsx
+++ src/components/features/editor/NoteEditor.jsx
@@ -18,7 +18,7 @@ export const NoteEditor = () => {
         value={content}
         onChange={(e) => setContent(e.target.value)}
         placeholder="Start typing here..."
-        className="textarea text-base textarea-ghost resize-none overflow-hidden w-full leading-relaxed focus:bg-transparent focus:outline-none "
+        className="textarea text-base textarea-ghost resize-none overflow-hidden w-full min-h-96 leading-relaxed focus:bg-transparent focus:outline-none "
       />
     </div>
   );
