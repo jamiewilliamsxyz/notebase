b9e7e84de0ee0e90d5145837cad903b8932802a8
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-14 12:35:31 +0100

    Style 404 not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,8 @@
 export const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page not found</h1>
+    <div className="h-screen flex flex-col gap-3 items-center justify-center">
+      <h1 className="font-bold text-5xl">404</h1>
+      <h2 className="font-semibold text-2xl">Page not found</h2>
     </div>
   );
 };
