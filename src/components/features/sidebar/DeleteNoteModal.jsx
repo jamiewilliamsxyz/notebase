caa91d9fb60d900746867ed6b8536b755b5e42e5
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-17 09:12:49 +0100

    Add close modal on overlay click and on cancel button click

--- src/components/features/sidebar/DeleteNoteModal.jsx
+++ src/components/features/sidebar/DeleteNoteModal.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { WorkspaceContext } from "../../../context/WorkspaceContext";
 
 export const DeleteNoteModal = () => {
-  const { isModalOpen } = useContext(WorkspaceContext);
+  const { isModalOpen, setIsModalOpen } = useContext(WorkspaceContext);
 
   return (
     <div
@@ -10,13 +10,21 @@ export const DeleteNoteModal = () => {
         isModalOpen ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
     >
-      <div className="absolute inset-0 bg-base-300/50"></div>
+      <div
+        onClick={() => setIsModalOpen(false)}
+        className="absolute inset-0 bg-base-300/50"
+      ></div>
       <div className="z-100 flex flex-col text-center gap-5 rounded shadow bg-base-100 p-5">
         <h3 className="font-semibold text-lg">
           Are you sure you want to delete this note?
         </h3>
         <div className="flex justify-center gap-5">
-          <button className="btn btn-soft">Cancel</button>
+          <button
+            onClick={() => setIsModalOpen(false)}
+            className="btn btn-soft"
+          >
+            Cancel
+          </button>
           <button className="btn btn-soft btn-error">Delete</button>
         </div>
       </div>
