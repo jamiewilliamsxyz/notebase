59829cebfe729d088b805fd001d91557dfd48c31
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-17 15:26:05 +0100

    Set sidebar initial state based on the screen width

--- src/context/LayoutContext.jsx
+++ src/context/LayoutContext.jsx
@@ -3,7 +3,12 @@ import { createContext, useState } from "react";
 export const LayoutContext = createContext();
 
 export const LayoutContextProvider = ({ children }) => {
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth >= 768;
+    }
+    return true;
+  });
 
   const toggleSidebar = () => {
     setIsExpanded(!isExpanded);
