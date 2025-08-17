f74af03734f3941c827a78e6a37ecc11231ba780
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-18 00:20:28 +0100

    Make save button responsive to sidebar open/close

--- src/components/features/editor/SaveButton.jsx
+++ src/components/features/editor/SaveButton.jsx
@@ -2,13 +2,17 @@ import { save } from "../../../utils/save";
 import { WorkspaceContext } from "../../../context/WorkspaceContext";
 import { Save } from "lucide-react";
 import { useContext } from "react";
+import { LayoutContext } from "../../../context/LayoutContext";
 
 export const SaveButton = () => {
   const { notes } = useContext(WorkspaceContext);
+  const { isExpanded } = useContext(LayoutContext);
 
   return (
     <button
-      className="tooltip tooltip-right cursor-pointer fixed bottom-1 left-2 w-fit h-fit p-2"
+      className={`tooltip tooltip-right cursor-pointer fixed bottom-1 left-2 w-fit h-fit p-2 transition-all duration-300 ease-in-out ${
+        isExpanded ? "ml-[20rem]" : ""
+      }`}
       data-tip="Save Note"
       onClick={save("notes", notes)}
     >
