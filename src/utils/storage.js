4c7f76839c8bacf10d3a164c22f20650541d22c7
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-18 10:43:40 +0100

    Move local storage functions to storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,3 +2,7 @@ export const get = (key) => {
   const value = localStorage.getItem(key);
   return value ? JSON.parse(value) : null;
 };
+
+export const save = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
