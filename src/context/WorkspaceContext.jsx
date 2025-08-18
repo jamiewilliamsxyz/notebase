bf5678f3f1d23d75b3ddd3ca448294b359218a59
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-18 09:37:32 +0100

    Remove unnessesary useEffect hook import

--- src/context/WorkspaceContext.jsx
+++ src/context/WorkspaceContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { get } from "../utils/get";
 import { save } from "../utils/save";
