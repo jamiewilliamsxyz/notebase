d558e0949628f8ca640fc73128c62b6be2fd3c3a
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 09:16:37 +0100

    Add aria-label

--- src/components/features/info/StartButton.jsx
+++ src/components/features/info/StartButton.jsx
@@ -5,6 +5,7 @@ export const StartButton = () => {
     <Link
       to="/"
       className="btn btn-primary max-w-44 fixed bottom-5 self-center"
+      aria-label="Open Editor Page"
     >
       Take me to the editor
     </Link>
