5b745219460026fd01bcab88e860281f86011756
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 13:14:39 +0100

    Ajust screenshot size

--- src/components/features/info/Screenshot.jsx
+++ src/components/features/info/Screenshot.jsx
@@ -1,3 +1,11 @@
 export const Screenshot = ({ image, alt }) => {
-  return <img src={image} alt={alt} className="rounded shadow" />;
+  return (
+    <img
+      src={image}
+      alt={alt}
+      width="45%"
+      height="45%"
+      className="rounded shadow"
+    />
+  );
 };
