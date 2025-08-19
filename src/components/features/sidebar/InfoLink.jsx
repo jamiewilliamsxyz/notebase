3c80cc155e47f34353ea90804f1e272dab69698b
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 15:51:51 +0100

    Ajust styles

--- src/components/features/sidebar/InfoLink.jsx
+++ src/components/features/sidebar/InfoLink.jsx
@@ -4,9 +4,14 @@ import { IconButton } from "../../ui/IconButton";
 
 export const InfoLink = () => {
   return (
-    <Link to="/info" aria-label="Info Page" className="mb-2 ml-2">
+    <Link
+      to="/info"
+      aria-label="Info Page"
+      className="bottom-2 left-2 tooltip tooltip-right w-fit h-fit"
+      data-tip="Info page"
+    >
       <IconButton>
-        <CircleQuestionMark className="w-6 h-6 bottom-2 left-2 text-primary" />
+        <CircleQuestionMark className="w-6 h-6 text-info" />
       </IconButton>
     </Link>
   );
