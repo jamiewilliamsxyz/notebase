58581c9b497a83427abaf36bd26f0f79d5f0fa88
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 15:57:08 +0100

    Ajust mobile sizing, position, padding & flex direction

--- src/components/features/editor/EditorToolbar.jsx
+++ src/components/features/editor/EditorToolbar.jsx
@@ -4,7 +4,7 @@ import { SaveButton } from "./SaveButton";
 
 export const EditorToolbar = ({ setIsAlertOpen, toggleEditorMode }) => {
   return (
-    <ul className="fixed top-2.5 right-2 flex flex-col sm:flex-row items-center gap-5 px-3 py-6 sm:gap-6.5 sm:px-6 sm:py-2.5 bg-base-100 rounded-xl shadow-lg">
+    <ul className="fixed top-1.5 right-1 sm:top-2.5 sm:right-2 flex flex-row items-center gap-5.5 px-4 py-2 sm:gap-6.5 sm:px-6 sm:py-2.5 bg-base-100 rounded-xl shadow-lg">
       <EditorModeToggle toggleEditorMode={toggleEditorMode} />
       <ThemeToggle />
       <SaveButton setIsAlertOpen={setIsAlertOpen} />
