ea718ccc8ff7433e649e16296ca9d077a5a2878c
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 11:10:09 +0100

    Ajust positioning

--- src/components/features/editor/EditorToolbar.jsx
+++ src/components/features/editor/EditorToolbar.jsx
@@ -4,7 +4,7 @@ import { SaveButton } from "./SaveButton";
 
 export const EditorToolbar = () => {
   return (
-    <ul className="fixed top-2.5 right-2.5 flex flex-col sm:flex-row items-center gap-5 px-3 py-6 sm:gap-6.5 sm:px-6 sm:py-2.5 bg-base-100 rounded-xl shadow-lg">
+    <ul className="fixed top-2.5 right-2 flex flex-col sm:flex-row items-center gap-5 px-3 py-6 sm:gap-6.5 sm:px-6 sm:py-2.5 bg-base-100 rounded-xl shadow-lg">
       <EditorModeToggle />
       <ThemeToggle />
       <SaveButton />
