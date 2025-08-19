70b158efabe5c56eff1adde29575cea609522530
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-08-19 09:33:34 +0100

    Add margin to be consistent with sidebar toolbar buttons

--- src/components/features/sidebar/InfoLink.jsx
+++ src/components/features/sidebar/InfoLink.jsx
@@ -4,7 +4,7 @@ import { IconButton } from "../../ui/IconButton";
 
 export const InfoLink = () => {
   return (
-    <Link to="/info" aria-label="Info Page">
+    <Link to="/info" aria-label="Info Page" className="mb-2 ml-2">
       <IconButton>
         <CircleQuestionMark className="w-6 h-6 bottom-2 left-2" />
       </IconButton>
